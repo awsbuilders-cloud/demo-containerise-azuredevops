67c627d59daa717c8560464092912c596b3b98c5
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-02-24 21:22:13 +1100

    Enable application deployment

--- infra/lib/containerMeetupApplication.js
+++ infra/lib/containerMeetupApplication.js
@@ -2,8 +2,6 @@ const cdk = require('@aws-cdk/core');
 const iam = require('@aws-cdk/aws-iam');
 const ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 const ecs = require('@aws-cdk/aws-ecs');
-const ec2 = require('@aws-cdk/aws-ec2');
-const autoScaling = require('@aws-cdk/aws-autoscaling');
 
 class ContainerMeetupApplication extends cdk.Stack {
     /**
@@ -17,18 +15,19 @@ class ContainerMeetupApplication extends cdk.Stack {
         // Create a load-balanced Fargate service and make it public
         const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
             serviceName: 'container-meetup-application',
-            cluster: props.ecsCluster, // Required
-            cpu: 256, // Default is 256
-            desiredCount: 3, // Default is 1
+            cluster: props.ecsCluster,
+            cpu: 256,
+            desiredCount: 3,
             taskImageOptions: {
                 containerName: 'container-meetup-application',
                 containerPort: 80,
                 image: ecs.ContainerImage.fromEcrRepository(props.ecr, process.env['GIT_COMMIT'] || 'latest')
             },
-            memoryLimitMiB: 512, // Default is 512
-            publicLoadBalancer: true // Default is false
+            memoryLimitMiB: 512,
+            publicLoadBalancer: true
         });
 
+        // Ensure that execution role has sufficient permissions to pull image from registry
         service.taskDefinition.addToExecutionRolePolicy(new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
             actions: ['ecr:*'],
