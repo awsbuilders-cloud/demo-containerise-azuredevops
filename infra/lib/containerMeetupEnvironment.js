a366b4d47f9e03680e652627d08314d8b135fc77
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-02-24 13:12:04 +1100

    Remove s3 bucket

--- infra/lib/containerMeetupEnvironment.js
+++ infra/lib/containerMeetupEnvironment.js
@@ -1,7 +1,6 @@
 const cdk = require('@aws-cdk/core');
 const ec2 = require('@aws-cdk/aws-ec2');
 const iam = require('@aws-cdk/aws-iam');
-const s3 = require('@aws-cdk/aws-s3');
 const secretsManager = require('@aws-cdk/aws-secretsmanager')
 const ecr = require('@aws-cdk/aws-ecr');
 const ecs = require('@aws-cdk/aws-ecs');
@@ -32,15 +31,6 @@ class ContainerMeetupEnvironment extends cdk.Stack {
     return user;
   }
 
-  _createBuildArtifactsBucket() {
-    return new s3.Bucket(this, 'BuildArtifacts', {
-      bucketName: 'container-meetup-build-artifacts',
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY 
-    });
-  }
-
   _createEcrRepository() {
     return new ecr.Repository(this, 'EcrRepository', {
       repositoryName: 'container-meetup-application',
@@ -64,7 +54,6 @@ class ContainerMeetupEnvironment extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
     this.vpc = new ec2.Vpc(this, 'ContainerMeetupVpc', { maxAzs: 3 });
-    this.buildArtifactsBucket = this._createBuildArtifactsBucket();
     this.buildPipelineUser = this._createBuildUser();
     this.ecr = this._createEcrRepository();
     this.ecsCluster = this._createEcsCluster();
